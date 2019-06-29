5ce15f71617586f7396a919614517bb29501ca71
v_pro <nkc.vadim.petrov@gmail.com>
2019-06-29 17:08:32 +0900

    commit 6

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,15 +1,78 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled" + " from users where username=?")
+				.authoritiesByUsernameQuery("select username, authority " + "from authorities where username=?")
+				.passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    //     http         
+    //     // .headers()
+    //     //  .frameOptions().sameOrigin()
+    //     //  .and()
+    //        .authorizeRequests()
+    //         .antMatchers("/resources/**", "/webjars/**","/css/**","/js/**","/img/**").permitAll()
+    //            .antMatchers("/login").permitAll()
+    //            .antMatchers("/error").permitAll()
+    //            .antMatchers("/autologin/**").permitAll()
+    //            .antMatchers("/admin/**").hasAuthority("ADMIN")
+    //            .antMatchers("/**").hasAnyAuthority("ADMIN","USER")
+    //            .anyRequest().authenticated()
+    //            .and()
+    //        .formLogin()
+    //            .loginPage("/login")
+    //            .defaultSuccessUrl("/kind-choose")
+    //            .failureUrl("/login?error")
+    //            .usernameParameter("loginId")
+    //            .passwordParameter("password")
+    //            .permitAll()
+    //            .and()
+    //             .rememberMe()
+    //             .tokenRepository(this.persistenceTokenRepository())
+    //             .tokenValiditySeconds(10000000)
+    //             .alwaysRemember(true)
+    //             .and()
+    //        .logout()
+    //         // .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+    //         .logoutSuccessUrl("/login?logout")
+    //         .permitAll()
+    //         .and()
+    //        .exceptionHandling()
+    //        .accessDeniedPage("/error")
+    //        .and()
+    //        .csrf().disable();
+    // }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -19,7 +82,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
-			.loginPage("/login").permitAll().successForwardUrl("/home");
+			.loginPage("/login").permitAll();
 			
 			//remember me configuration
 			http
@@ -31,12 +94,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.logoutUrl("/logout");
 			
 	}
-	
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-				.withUser("websparrow").password("{noop}web123").roles("USER");
-				//.withDefaultPasswordEncoder().username("user").password("pass").roles("user").build();
-	}	
+
+	@Bean
+	public PersistentTokenRepository tokenRepository() {
+		JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl = new JdbcTokenRepositoryImpl();
+		jdbcTokenRepositoryImpl.setDataSource(dataSource);
+		return jdbcTokenRepositoryImpl;
+	}
 }
