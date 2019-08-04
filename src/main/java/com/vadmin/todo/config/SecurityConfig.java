31a0797fac84e954f5af9e051f0681d0172b97d1
isacRU11 <vwso.bcl@gmail.com>
2019-08-04 23:49:05 +0900

    gcp 1

--- src/main/java/com/vadmin/todo/config/SecurityConfig.java
+++ src/main/java/com/vadmin/todo/config/SecurityConfig.java
@@ -1,75 +1,46 @@
 package com.vadmin.todo.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
 	@Autowired
-    private DataSource dataSource;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-				.usersByUsernameQuery("select username, password, enabled" + " from users where username=?")
-				.authoritiesByUsernameQuery("select username, authority " + "from authorities where username=?")
-				.passwordEncoder(passwordEncoder());
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("webuser").password("{noop}web123").roles("ADMIN");
 	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-    }
     
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.csrf().disable()
+		   .csrf().disable()
            .authorizeRequests()
             .antMatchers("/resources/**", "/webjars/**","/css/**","/js/**","/img/**").permitAll()
                .antMatchers("/login").permitAll()
-               .antMatchers("/**").hasAnyAuthority("ROLE_ADMIN","USER")
                .anyRequest().authenticated()
                .and()
-           .formLogin()
-               .loginPage("/login")
-               .defaultSuccessUrl("/todo")
-               .failureUrl("/login?error")
-               .usernameParameter("username")
-               .passwordParameter("password")
-               .permitAll();
+			   .formLogin()
+			   .loginPage("/login").permitAll();
 			
-			//remember me configuration
-			http
-				.rememberMe().tokenRepository(this.tokenRepository())
-				.tokenValiditySeconds(10000000).alwaysRemember(true);
 			
-			http
-				.logout()
-                .logoutUrl("/logout");
-                
-            http.exceptionHandling()
-                .accessDeniedPage("/error");
+		http
+		.rememberMe()
+		.rememberMeCookieName("remember-me")
+		.tokenValiditySeconds(10000000);
+			
+		http
+			.logout()
+			.logoutUrl("/logout");
 			
 	}
 
-	@Bean
-	public PersistentTokenRepository tokenRepository() {
-		JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl = new JdbcTokenRepositoryImpl();
-		jdbcTokenRepositoryImpl.setDataSource(dataSource);
-		return jdbcTokenRepositoryImpl;
-	}
 }
